d51dfd8d407383ea4cd87239b1ecbc9f6ba09a84
azeem <kaziazeemsiddiqui786@gmail.com>
2021-05-26 00:13:51 +0530

    day2 commit

---
+++ src/VariablesDataTypes_Day2.kt
@@ -0,0 +1,36 @@
+var id = 2
+var name = "Azeem"
+
+fun main() {
+
+    /*  var days:Int=7
+      days=10
+
+      val month:String="January"
+      month="May"
+
+      println(days)
+      println(month)*/
+
+    var section = 'B'
+    var largestMarks = 999L
+    var percentage = 95.6F
+    var distance = 225.5
+    var isLogin = true
+
+    var number:Number
+    number=56
+    number=6.9
+    number=3.0F
+    number=9999L
+
+    println(id)
+    println(name)
+    println(section)
+    println(largestMarks)
+    println(percentage)
+    println(distance)
+    println(isLogin)
+
+
+}
