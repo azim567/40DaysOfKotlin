e0be9bfbbe297f4f1ab5a5ceb3da1dcb5de2d6d2
azeem <kaziazeemsiddiqui786@gmail.com>
2021-05-26 01:59:47 +0530

    day4 commit

---
+++ src/IfAsExpression_Day4.kt
@@ -0,0 +1,20 @@
+fun main() {
+
+    var a=8
+    var b=5
+
+  /*  var max=b
+
+    max = if(a>b){
+        a
+    }
+    else{
+        b
+    }
+
+    print(max)*/
+
+    var result= if(a>b) a else b
+
+    print(result)
+}
