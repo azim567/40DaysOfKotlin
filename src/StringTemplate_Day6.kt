924240a8ed500d9ace76b10d3d4a7ab8d224bd01
azeem <kaziazeemsiddiqui786@gmail.com>
2021-05-26 22:51:15 +0530

    day6 commit

---
+++ src/StringTemplate_Day6.kt
@@ -0,0 +1,12 @@
+fun main() {
+
+    var name="Rohan"
+    var device="Computer"
+    var quantity=5
+
+    //print(name+" "+"has"+" "+quantity+" "+device)
+
+    println("$name has $quantity $device")
+    print("length of name is: ${name.length}")
+
+}
